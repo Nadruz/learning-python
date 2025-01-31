b32870b1aea9de8043c3273e0a369a3ca4d0d8da
Nadruz <juliananadruz@gmail.com>
2025-01-31 17:24:43 -0300

    Utilizando repetidores - FOR

---
+++ repetidores-while.py
@@ -0,0 +1,14 @@
+# Utilizando while
+notas = []
+
+contador = 1 
+
+while contador <= 5:
+    codigo_aluno = input("RM: ")
+    nota = float(input("Nota: "))
+    resultado = [codigo_aluno, nota]
+    notas.append(resultado)
+
+contador = contador + 1
+
+print("Quantidade de notas", len(notas))
